4a24efed143209e87a1a8ae4d4d60329cfa3d4c3
jimmykodes <jimmy@khanacademy.org>
2024-09-23 12:12:35 -0700

    feat: remove colorscheme shenanigans

--- lua/jk/color.lua
+++ lua/jk/color.lua
@@ -1,21 +1,7 @@
 local M = {}
 
-local function _get_color()
-	local color = "system76"
-	local conf = os.getenv("XDG_CONFIG_HOME")
-	if conf == nil then
-		return color
-	end
-	local f = io.open(conf .. "/color", "r")
-	if f == nil then
-		return color
-	end
-	color = f:read()
-	return color
-end
-
 function M.setup()
-	local color = _get_color()
+	local color = "system76"
 	vim.g.colors_name = color
 	vim.cmd("colorscheme " .. color)
 end
