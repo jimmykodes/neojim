07b9d75854f27f584b5433ada7b11014d672fb79
jimmykodes <jimmy@khanacademy.org>
2025-03-27 10:01:10 -0700

    feat: custom tag add/remove

--- lua/jk/ftplugin/go.lua
+++ lua/jk/ftplugin/go.lua
@@ -24,6 +24,17 @@ local M = {
 					t = {
 						a = ':GoTagAdd<CR>',
 						r = ':GoTagRm<CR>',
+						c = function()
+							vim.ui.input({ prompt = "Tag Name" }, function(input)
+								require("gopher.struct_tags").add(input)
+							end)
+						end,
+						C = function()
+							vim.ui.input({ prompt = "Tag Name" }, function(input)
+								require("gopher.struct_tags").remove(input)
+							end)
+						end
+
 					},
 					m = {
 						t = ':GoMod tidy<CR>',
@@ -61,24 +72,6 @@ local M = {
 		end
 	}
 }
-
-
--- require("which-key").register({
--- 	G = {
--- 		name = "Go",
--- 		i = { "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies" },
--- 		t = { "<cmd>GoMod tidy<cr>", "Tidy" },
--- 		a = { "<cmd>GoTestAdd<Cr>", "Add Test" },
--- 		A = { "<cmd>GoTestsAll<Cr>", "Add All Tests" },
--- 		E = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
--- 		g = { "<cmd>GoGenerate<Cr>", "Go Generate" },
--- 		f = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
--- 		c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
--- 		e = { "<cmd>GoIfErr<Cr>", "If err" },
--- 		T = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" },
--- 	},
--- }, { prefix = "<leader>" })
-
 
 ---@param opts FTOpts?
 function M.setup(opts)
