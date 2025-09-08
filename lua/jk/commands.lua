87ef753e7c3050959fdadfc2d2e08ddb4616d27d
jimmykodes <jimmy@khanacademy.org>
2025-09-08 07:29:03 -0700

    feat: add Date command and keymap

--- lua/jk/commands.lua
+++ lua/jk/commands.lua
@@ -10,11 +10,20 @@ local M = {
 		endfunction
 		]],
 	},
+	lua = {
+		["Date"] = function()
+			local date = os.date('%Y-%m-%d')
+			vim.api.nvim_put({ date }, 'c', true, true)
+		end,
+	}
 }
 
 function M.setup()
 	for _, cmd in ipairs(M.cmds) do
 		vim.cmd(cmd)
+	end
+	for cmd, body in pairs(M.lua) do
+		vim.api.nvim_create_user_command(cmd, body, {})
 	end
 end
 
--- lua/jk/keymaps.lua
+++ lua/jk/keymaps.lua
@@ -24,6 +24,7 @@ M.config = {
 					vim.snippet.jump(-1)
 				end
 			end,
+			["<C-d>"] = "<esc><cmd>Date<CR>i",
 		},
 		t = {
 			['<ESC><ESC>'] = [[<C-\><C-N>]],
